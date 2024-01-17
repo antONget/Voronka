bfd755e16762a7490754984d67ed5c3b5044e97a
antONget <a.l.ponomare@mail.ru>
2024-01-17 23:24:38 +0300

    link paper2

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,5 +1,7 @@
 from aiogram import Router, F
 from aiogram.types import Message
+import logging
+from aiogram.types import CallbackQuery
 
 router = Router()
 
@@ -7,3 +9,7 @@ router = Router()
 @router.message(F.photo)
 async def process_start_command(message: Message) -> None:
     print(message.photo[3].file_id)
+
+@router.callback_query(lambda callback: callback.data != 's')
+async def callback_data(callback: CallbackQuery) -> None:
+    logging.info(f'callback_data: {callback.data}')
